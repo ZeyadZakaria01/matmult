e0a7062624b1d9faccc3c218f705a0ddec0c294c
Zeyad Zakaria <zeyad.zakaria01@gmail.com>
2022-12-30 02:49:15 +0200

    Adding Comments

--- matmult.c
+++ matmult.c
@@ -37,6 +37,7 @@ typedef struct arguments
     int i, j, l, n, m;
 } arguments;
 
+// Function to return a pointer to the argument with assigned value
 arguments *get_arguments_ptr(int *A, int *B, int *C, int l, int m, int n, int i, int j)
 {
     arguments *args = malloc(sizeof(arguments));
@@ -52,10 +53,13 @@ arguments *get_arguments_ptr(int *A, int *B, int *C, int l, int m, int n, int i,
  *     A has l rows and m cols
  *     B has m rows and n cols
  *     C has l rows and n cols
+ *
+ * routine_v1 creates a thread for each element
  */
 void *routine_v1(void *arg)
 {
     arguments args = *(arguments *)arg;
+    // Calculating element of multiplied matrix c[i][j]
     int sum = 0;
     for (int k = 0; k < args.m; k++)
         sum += Item(args.A, args.i, k, args.m) * Item(args.B, k, args.j, args.n);
@@ -65,31 +69,40 @@ void *routine_v1(void *arg)
 
 void matmult_v1(int *A, int *B, int *C, int l, int m, int n)
 {
+    // Number of threads = number of elements in matrix C
     pthread_t th[l][n];
 
+    // Loop on rows
     for (int i = 0; i < l; i++)
     {
+        // Loop on columns
         for (int j = 0; j < n; j++)
         {
+            // get pointer to arguments to be passed to thread
             arguments *args = get_arguments_ptr(A, B, C, l, m, n, i, j);
+            // create a thread for each element in matrix C
             if (pthread_create(&th[i][j], NULL, &routine_v1, args) != 0)
             {
-                printf("args=%p\n[%d][%d]: ", args, i, j);
                 perror("Failed to create thread\n");
                 pthread_exit(0);
             }
         }
     }
+
+    // Loop on rows
     for (int i = 0; i < l; i++)
     {
+        // Loop on columns
         for (int j = 0; j < n; j++)
         {
             arguments *args;
+            // Wait for each thread to be finished first
             if (pthread_join(th[i][j], (void **)&args) != 0)
             {
                 perror("Failed to join thread\n");
                 pthread_exit(0);
             }
+            // Free pointers
             free(args);
         }
     }
@@ -101,13 +114,16 @@ void matmult_v1(int *A, int *B, int *C, int l, int m, int n)
  *     A has l rows and m cols
  *     B has m rows and n cols
  *     C has l rows and n cols
+ *
+ * routine_v2 creates a thread for each row
  */
-
 void *routine_v2(void *arg)
 {
     arguments args = *(arguments *)arg;
+    // Loop on columns
     for (int j = 0; j < args.n; j++)
     {
+        // Calculating element of multiplied matrix c[i][j]
         int sum = 0;
         for (int k = 0; k < args.m; k++)
             sum += Item(args.A, args.i, k, args.m) * Item(args.B, k, j, args.n);
@@ -115,30 +131,37 @@ void *routine_v2(void *arg)
     }
     pthread_exit(arg);
 }
+
 void matmult_v2(int *A, int *B, int *C, int l, int m, int n)
 {
+    // Number of threads = number of rows in matrix C
     pthread_t th[l];
 
+    // Loop on rows
     for (int i = 0; i < l; i++)
     {
-
+        // get pointer to arguments to be passed to thread
         arguments *args = get_arguments_ptr(A, B, C, l, m, n, i, 0);
+        // create a thread for each row in matrix C
         if (pthread_create(&th[i], NULL, &routine_v2, args) != 0)
         {
-            printf("args=%p\n[%d]: ", args, i);
+            // printf("args=%p\n[%d]: ", args, i);
             perror("Failed to create thread\n");
             pthread_exit(0);
         }
     }
 
+    // Loop on rows
     for (int i = 0; i < l; i++)
     {
         arguments *args;
+        // Wait for each thread to be finished first
         if (pthread_join(th[i], (void **)&args) != 0)
         {
             perror("Failed to join thread\n");
             pthread_exit(0);
         }
+        // Free pointers
         free(args);
     }
 }
